3debb7768468ccf991fbb1510362e245c589ca43
samu-perez <3607487240101@ingenieria.usac.edu.gt>
2023-02-19 22:57:49 -0600

    Se corrigió el endpoint calcular-suma por calcular-multiplicacion
    Recibo como parámetros dos números y devuelve la multiplicación

--- src/controllers/calculadora.controller.js
+++ src/controllers/calculadora.controller.js
@@ -1,15 +1,15 @@
-const calcularSuma = (req, res) => {
+const calcularMultiplicacion = (req, res) => {
     const {num1, num2} = req.body
 
     try {
-        const resultado = parseInt(num1) + parseInt(num2)        
+        const resultado = parseInt(num1) * parseInt(num2)        
         res.status(200).json({resultado: resultado})
     } catch (error) {
         console.error('Error calculadora - calculadora.controller', error)
-        res.status(500).json({message: "Error al sumar"})
+        res.status(500).json({message: "Error al multiplicar"})
     }
 }
 
 module.exports = {
-    calcularSuma
+    calcularMultiplicacion
 }
